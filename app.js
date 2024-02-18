a75b0d486b010e888eb1b1872d9dddf951fb9542
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-02-18 14:39:09 +0600

    seatButton Update

--- app.js
+++ app.js
@@ -21,31 +21,41 @@ for (let btn of seatButtons) {
     //     console.log(indicatorValue++);
     //seat- update
     const seatUpdate = document.querySelector(".seat-update");
-    const p = document.createElement("p");
-    p.innerText = e.target.innerText;
-    seatNumber.appendChild(p);
-    const p2 = document.createElement("p");
-    p2.innerText = "Economy";
-    seatClass.appendChild(p2);
-    const p3 = document.createElement("p");
-    p3.innerText = "550";
-    seatPrice.appendChild(p3);
-    console.log(e.target.style);
+   
+    appendParagraph(seatNumber, e.target.innerText);
+    appendParagraph(seatClass, "Economy");
+    appendParagraph(seatPrice, "550");
+    //     console.log(e.target.style);
     e.target.classList.toggle("bg-green-500");
+    e.target.classList.toggle("cursor-not-allowed");
+    e.target.classList.toggle("pointer-events-none");
+
     //     e.target.classList.toggle("btn-disabled");
 
-    //Proce update
+    //Price update
     //     let totalPrice = parseInt(document.querySelector("#total-price").innerText);
     // //     console.log(totalPrice)
     //    let totalPriceValue = parseInt(seatPrice.innerText);
     //     console.log(totalPriceValue);
     let totalPrice = parseInt(document.querySelector("#total-price").innerText);
     console.log(totalPrice);
-    total = totalPrice + parseInt(p3.innerText);
+    total = totalPrice + parseInt(550);
     document.querySelector("#total-price").innerText = total;
     //     console.log(parseInt(p3.innerText));
     //     let totalPriceCost = total + parseInt(p3.innerText);
     //     console.log(totalPriceCost)
     //     totalPrice.innerText = totalPriceCost++;
+
+    //grand total;
+    document.querySelector("#grand-total-price").innerText = total;
+
+    //discounted Price
   });
+}
+
+//append Paragraph
+function appendParagraph(mainElement, text) {
+  const p = document.createElement("p");
+  p.innerText = text;
+  mainElement.appendChild(p);
 }
